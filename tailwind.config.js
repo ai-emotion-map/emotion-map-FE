fb38b1dfc4365b02f3a2f7a459e75ef74ff945ec
xuuwon <dumpling1011@gmail.com>
2025-08-10 21:32:27 +0900

    style: tailwind.config에 프로젝트 커스텀 컬러 팔레트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "main-green": "#6FCF97",
         "sub-green": "#B7E0C2", // 연한 그린
         background: "#FAFAFA", // 배경색
+        "marker-yellow": "#fff386",
+        "marker-blue": "#a2beff",
+        "marker-orange": "#ff897d",
+        "marker-purple": "#c26dde",
+        "marker-pink": "#ff9fc4",
+        "marker-green": "#8fd298",
       },
     },
   },
