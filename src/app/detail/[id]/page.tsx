da9899b9a0e5e11c45117fbe0ec7b386289fcaa3
xuuwon <dumpling1011@gmail.com>
2025-08-24 04:36:41 +0900

    fix: [id] 페이지 타입 에러 수정

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -2,12 +2,15 @@ import { Api } from "@/app/api/api";
 import React from "react";
 import { DetailClient } from "./client";
 
-const Page = async ({ params }: { params: { id: string } }) => {
+type Params = {
+  id: string;
+};
+
+const Page = async ({ params }: { params: Params }) => {
   const id = Number(params.id);
 
   // 서버에서 마커 데이터 가져오기
   const markersData = await Api.getPostById(id);
-  console.log(markersData);
 
   return <DetailClient markersData={markersData} />;
 };
