5dea25199de81d578ba680c8219e1816cb442f65
xuuwon <dumpling1011@gmail.com>
2025-08-24 04:34:04 +0900

    fix: 서버 페이지 컴포넌트 타입 충돌 해결 및 params 직접 구조분해 적용

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -2,11 +2,7 @@ import { Api } from "@/app/api/api";
 import React from "react";
 import { DetailClient } from "./client";
 
-interface PageProps {
-  params: { id: string };
-}
-
-const Page = async ({ params }: PageProps) => {
+const Page = async ({ params }: { params: { id: string } }) => {
   const id = Number(params.id);
 
   // 서버에서 마커 데이터 가져오기
