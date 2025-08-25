47f55aa70c7c45b6c9a2b390313b40a5e3dbdd10
xuuwon <dumpling1011@gmail.com>
2025-08-25 20:02:43 +0900

    style: detail 페이지 헤더 그림자 추가

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import clsx from "clsx";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <header className="flex items-center h-[80px] pl-4 bg-background">
+    <header
+      className={clsx(
+        "flex items-center h-[80px] pl-4 z-50 bg-background",
+        pathname.startsWith("/detail") &&
+          "drop-shadow-[0_4px_4px_rgba(183,224,194,0.3)]"
+      )}
+    >
       <Image
         src="/images/clustory.png"
         alt="Logo"
