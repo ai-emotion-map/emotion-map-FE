f0cdb79eed2bac99a0d4cf06cf06ddb6e3dffc41
xuuwon <dumpling1011@gmail.com>
2025-08-25 20:40:33 +0900

    feat: 추천 코스 페이지 텍스트 추가

--- src/app/course/recommend/client.tsx
+++ src/app/course/recommend/client.tsx
@@ -12,6 +12,7 @@ import {
   TagVariant,
 } from "@/app/components/common/tag/tag.types";
 import { RecommendedCourse, Stop } from "./recommend.types";
+import { emotionImages } from "@/app/components/navermap/naverMap.types";
 
 const RecommendCourseClient = () => {
   const searchParams = useSearchParams();
@@ -35,18 +36,49 @@ const RecommendCourseClient = () => {
   if (!courseData)
     return (
       <div className="h-[calc(100vh-150px)] flex flex-col items-center justify-center gap-3 pt-10 text-sm text-center text-gray-500">
-        <p> 코스를 가져오는 중입니다.</p>  {/*  ♾️ 제거 (다시넣어도 돼요) */}
-        
+        <p> 코스를 가져오는 중입니다.</p>
+
         <Loading />
       </div>
     );
 
   return (
-    <div className="flex flex-col h-full gap-3">
+    <div className="flex flex-col items-center h-full gap-3">
+      <div className="relative flex flex-col items-center w-full">
+        {/* 도트선 */}
+        <svg
+          width="100%"
+          height="10"
+          className="absolute left-0 z-0 top-1/2"
+          style={{ transform: "translateY(-50%)" }}
+        >
+          <line
+            x1="0"
+            y1="5"
+            x2="100%"
+            y2="5"
+            stroke="#C0E5D3"
+            strokeWidth="2"
+            strokeDasharray="14,14"
+            strokeLinecap="round"
+          />
+        </svg>
+        {/* 태그 박스 */}
+        <span className="text-white z-10 flex items-center gap-2 px-4 py-1.5 rounded-full bg-main-green">
+          {REVERSE_TAG_MAP[tag as TagVariant]} 코스 추천
+          <img
+            src={`${emotionImages[tag as TagVariant]}`}
+            alt="코스 아이콘"
+            width={25}
+            height={25}
+            className="inline-block align-middle"
+          />
+        </span>
+      </div>
       <NaverMap
         markers={markers}
         zoom={14}
-        height="40vh"
+        height="35vh"
         polyline={courseData?.polyline}
         center={
           markers.length
@@ -54,7 +86,7 @@ const RecommendCourseClient = () => {
             : undefined
         }
       />
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col w-full gap-2">
         {courseData.stops.map((stop, idx) => (
           <div
             key={idx}
